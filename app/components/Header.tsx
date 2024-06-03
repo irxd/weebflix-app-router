b96ba0fdb1decf97ae5d18907828d73c94b7beb0
irxd <irsyart@gmail.com>
2024-06-03 16:53:48 +0700

    feat: add material ui and header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,23 @@
+import { SearchOutlined } from "@mui/icons-material";
+import { AppBar, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <AppBar position="static" sx={{ height: "80px", backgroundColor: "black" }}>
+      <Container
+        sx={{
+          display: "flex",
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "space-between"
+        }}>
+        <Image src="/logo.png" alt="logo" width={120} height={36} />
+        <Stack direction="row" gap="16px">
+          <SearchOutlined />
+          <Typography>My Favorite</Typography>
+        </Stack>
+      </Container>
+    </AppBar>
+  );
+}
